d6864c311e5dbb41042c29dc57c04d8ace71d9d0
Ritu Bhardwaj <ritubhardwaj0410@gmail.com>
2024-08-05 07:45:06 -0400

    Pracrice

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 , specification: 'Coding'},
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 , specification: 'Python'},
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specification: 'Python' },
     //... More employee records can be added here
   ];
 
@@ -33,3 +33,13 @@ function findEmployeeById(employeeId) {
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
 
       }}
+
+function findEmployeeBySpecification(specs) {
+    const foundEmployee = employees.find(employee => employee.specification.toLowerCase() === specs.toLowerCase);
+        if (foundEmployee) {
+            document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+        }      
+        else{
+            document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this Specification';
+
+        }}
